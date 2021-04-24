b5e489ff42c1d3dd1e38d431c086a263cb64c238
rahim2016 <rahimpichou@yahoo.fr>
2021-04-24 09:35:53 +0100

    U

--- WhiteCow/settings.py
+++ WhiteCow/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        'USER': 'postgres',
+        'PASSWORD': 'password',
         'HOST': os.environ.get('DB_HOST'),
     }
 }
