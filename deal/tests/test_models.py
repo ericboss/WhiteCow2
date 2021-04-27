3901c8023d6dd88431fc316f0ec1170d172fa5cf
Tagne Nguifo Eric Hermann <ericnguifo@gmail.com>
2021-04-27 10:50:52 +0100

    updates11

--- deal/tests/test_models.py
+++ deal/tests/test_models.py
@@ -1,49 +1,4 @@
-from django.test import TestCase, Client
-from deal.models import Deals, ComputeDeals, Adress,AssetTypes
-import json
-from django.contrib.auth.models import User
-from deal.config import *
-import unittest
-from unittest.mock import patch
-import mock
 
-import os
-
-class TestModels(TestCase):
-
-    def setUp(self):
-        self.address1 = Adress.objects.create(city="New York City", state_code="NY")
-        self.asset1 = AssetTypes.objects.create()
-        self.compute1 = ComputeDeals.objects.create(period= 1, compare='below', percentage_compare_average_price=0)
-        self.user = User.objects.create_user(username='testuser', email='test@company.com', password='12345')
-        self.deal1 = Deals.objects.create(user=self.user, name='deal1', property_status='For Rent', adress=self.address1, assets=self.asset1, computeDeal=self.compute1)
-        
-        self.pk = self.deal1.pk
-    
-    def test_get_query_params(self):
-        
-
-        params = {'city': 'New York City', 'state_code': 'NY', 'postal_code': '', 'offset': '0', 'limit': 10, 'baths_min': None, 'beds_min': None, 'radius': None, 'price_min': None, 'sqft_min': None, 'age_min': None, 'lot_sqft_max': None, 'price_max': None, 'lot_sqft_min': None, 'prop_type': '', 'age_max': None, 'sort': '', 'sqft_max': None}
-        self.assertEqual(self.deal1.get_query_params(), params )
-
-  #  def test_search_query_GET(self):
-   #     response = self.deal1.property_search_query(url_for_rent)
-    #    self.assertEqual(response.status_code, 200)
-
-        
-   
-    def test_property_search_query(self):
-        fake_response = [{'meta': {'build': '3.23.144',}}]
-        with patch("deal.models.Deals.property_search_query") as mock_response:
-            mock_response.return_value.ok = True
-            mock_response.return_value.json.return_value = fake_response
-            self.assertEqual(self.deal1.property_search_query(url_for_rent).ok,True )
-            self.assertEqual(self.deal1.property_search_query(url_for_rent).json(),fake_response)
-
-            mock_response.return_value.ok = False
-            
-            self.assertEqual(self.deal1.property_search_query(url_for_rent).ok,False )
-        
 
     
 
--- deal/tests/test_views.py
+++ deal/tests/test_views.py
@@ -1,28 +1,29 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from deal.models import Deals, ComputeDeals, Adress,AssetTypes
+from deal.models import Deals, Adress,AssetsForSale
 import json
 from django.contrib.auth.models import User
-from deal.forms import DealAddressForm, DealAssetTypeForm, DealComputeDealForm, DealsForm
+
 
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.index_url = reverse('index')
-        self.subscriptions_url = reverse('subscriptions')
-        self.display_url = reverse('display')
-        self.deals_new_url = reverse('deals-new')
-        self.address1 = Adress.objects.create(city="New York City", state_code="NY", postal_code="")
-        self.asset1 = AssetTypes.objects.create()
-        self.compute1 = ComputeDeals.objects.create(period=1, compare='below', percentage_compare_average_price=0)
-        self.user = User.objects.create_user(username='testuser', email='test@company.com', password='12345')
-        self.deal1 = Deals.objects.create(user=self.user, name='deal1', property_status='For Rent', adress=self.address1, assets=self.asset1, computeDeal=self.compute1)
-        
-        self.pk = self.deal1.pk
+        self.add_deal_url = reverse('add-deal')
+        self.address_assets_url = reverse('address_assets')
+        self.save_deal_url = reverse('save_deal')
 
-        self.edit_deal_url = reverse('edit', args=[self.pk])
-        self.delete_url = reverse('delete', args = [self.pk])
-        self.display_specific_url = reverse('display_specific', args=[self.pk])
+       # self.address1 = Adress.objects.create(city="New York City", state_code="NY", postal_code="")
+       # self.asset1 = AssetsForSale.objects.create()
+        
+       # self.user = User.objects.create_user(username='testuser', email='test@company.com', password='12345')
+       # self.deal1 = Deals.objects.create(owner=self.user, name='deal1', property_status='Sale')
+        
+       # self.pk = self.deal1.pk
+
+       # self.deal_detail_url = reverse('deal-detail', args=[self.pk])
+        #self.delete_url = reverse('deal_delete', args = [self.pk])
+     
 
     def test_index_GET(self):
 
@@ -30,117 +31,7 @@ class TestViews(TestCase):
         response = self.client.get(self.index_url)
 
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'deal/index.html')
-
-    def test_subscriptions_GET(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        response = self.client.get(self.subscriptions_url)
-        
-
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'deal/subscriptions.html')
-
-    def test_display_GET(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        response = self.client.get(self.display_url)
-        
-
-
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'deal/display.html')
-
-    def test_deals_new_GET(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        response = self.client.get(self.deals_new_url)
-
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'deal/deal-form.html')
-
-    def test_edit_GET(self):
-        
-        login = self.client.login(username='testuser', password='12345')
-        response = self.client.get(self.edit_deal_url)
-
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'deal/edit.html')
+        self.assertTemplateUsed(response, 'deal/Views/dashboard.html')
 
     
-    def test_delete_GET(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        response = self.client.get(self.delete_url)
-
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'deal/delete.html')
-
-    def test_display_specific_GET(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        response = self.client.get(self.display_specific_url)
-
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'deal/display_specific.html')
     
-    def test_edit_POST(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        deal = Deals.objects.get(pk = self.pk)
-        deal_form = DealsForm( instance=deal)
-        response = self.client.post(self.edit_deal_url, {'edit_form':deal_form, 'deal':deal})
-
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(self.deal1.name, 'deal1')
-
-    def test_specific_deal_POST(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        deal = Deals.objects.get(pk = self.pk)
-        address = Adress.objects.get(pk = self.pk) 
-        asset = AssetTypes.objects.get(pk = self.pk) 
-        compute = ComputeDeals.objects.get(pk = self.pk)
-
-
-        deal_form = DealsForm( instance=deal)
-        form_adress = DealAddressForm(instance=address)
-        form_compute = DealComputeDealForm(instance=compute)
-        form_asset = DealAssetTypeForm(instance=asset)
-
-        response = self.client.post(self.display_specific_url, {'deal':deal,'deal_form': deal_form,'adress':form_adress, 'compute':form_compute, 'asset':form_asset})
-
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(self.deal1.name, 'deal1')
-
-
-    def test_create_deal_POST(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        deal = Deals.objects.get(pk = self.pk)
-        address = Adress.objects.get(pk = self.pk) 
-        asset = AssetTypes.objects.get(pk = self.pk) 
-        compute = ComputeDeals.objects.get(pk = self.pk)
-
-
-        deal_form = DealsForm(data={'name':'deal2', 'property_status':'For Rent'})
-        form_adress = DealAddressForm(instance=address)
-        form_compute = DealComputeDealForm(instance=compute)
-        form_asset = DealAssetTypeForm(instance=asset)
-
-        response = self.client.post(self.deals_new_url, {'deal':deal,'deal_form': deal_form,'adress':form_adress, 'compute':form_compute, 'asset':form_asset})
-        
-        self.assertEquals(response.status_code, 200)
-       
-       
-      
-    
-    def test_delete_POST(self):
-        login = self.client.login(username='testuser', password='12345')
-
-        deal = Deals.objects.get(pk = self.pk)
-        
-        response = self.client.post(self.delete_url, {'deal':deal})
-
-        self.assertEquals(response.status_code, 302)
-        
