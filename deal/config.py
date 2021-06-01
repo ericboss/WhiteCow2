84c15f3209f2a3e7246310185b9f49c574faf1d7
Tagne Nguifo Eric Hermann <ericnguifo@gmail.com>
2021-06-01 13:44:46 +0100

    updates address_asset.html

--- deal/config.py
+++ deal/config.py
@@ -1,4 +1,4 @@
-api_key="0337507891msh492c3e9c4683fc2p19f021jsn8671df628f1c"
+api_key="aecb438fd4mshcbfaff7d555143bp109ee0jsn81774e92b1a4"
 host = "us-real-estate.p.rapidapi.com"
 url_for_sale = "https://us-real-estate.p.rapidapi.com/for-sale"
 url_for_rent = "https://us-real-estate.p.rapidapi.com/for-rent"
